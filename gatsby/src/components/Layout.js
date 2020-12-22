f2371399fb24ff902cc157353cb5b308a41ac7b5
Chris Arsenault <chrisstanarsenault@gmail.com>
2020-12-22 01:43:02 -0500

    Added basic styling.  Setup Sanity CMS and created data relationships.  Started working with GraphQL queries

--- gatsby/src/components/Layout.js
+++ gatsby/src/components/Layout.js
@@ -1,13 +1,48 @@
 import React from 'react';
+import styled from 'styled-components';
+
 import Footer from './Footer';
 import Nav from './Nav';
 
+import stripes from '../assets/images/stripes.svg';
+
+import 'normalize.css';
+import GlobalStyles from '../styles/GlobalStyles';
+import Typography from '../styles/Typography';
+
+const ContentStyles = styled.div`
+  background: white;
+  padding: 2rem;
+`;
+
+const SiteBorderStyles = styled.div`
+  max-width: 1000px;
+  margin: 12rem auto 4rem auto;
+  margin-top: clamp(2rem, 10vw, 12rem);
+  padding: 5px;
+  padding: clamp(5px, 1vw, 25px);
+  box-shadow: 0 0 5px 3px rgba(0, 0, 0, 0.044);
+  background: white url(${stripes});
+  background-size: 1500px;
+  border: 5px solid white;
+  @media (max-width: 1100px) {
+    margin-left: 1.5rem;
+    margin-right: 1.5rem;
+  }
+`;
+
 export default function Layout(props) {
   return (
-    <div>
-      <Nav />
-      {props.children}
-      <Footer />
-    </div>
+    <>
+      <GlobalStyles />
+      <Typography />
+      <SiteBorderStyles>
+        <ContentStyles>
+          <Nav />
+          {props.children}
+          <Footer />
+        </ContentStyles>
+      </SiteBorderStyles>
+    </>
   );
 }
--- gatsby/src/pages/pizzas.js
+++ gatsby/src/pages/pizzas.js
@@ -1,10 +1,38 @@
 import React from 'react';
-import Layout from '../components/Layout';
+import { graphql } from 'gatsby';
 
-export default function PizzasPage() {
+import PizzaList from '../components/PizzaList';
+
+export default function PizzasPage({ data }) {
+  const pizzas = data.pizzas.nodes;
   return (
     <>
-      <p>Hey! I'm the Pizzas page!</p>
+      <PizzaList pizzaList={pizzas} />
     </>
   );
 }
+
+export const query = graphql`
+  query PizzaQuery {
+    pizzas: allSanityPizza {
+      nodes {
+        name
+        id
+        slug {
+          current
+        }
+        toppings {
+          id
+          name
+        }
+        image {
+          asset {
+            fluid(maxWidth: 400) {
+              ...GatsbySanityImageFluid
+            }
+          }
+        }
+      }
+    }
+  }
+`;
